7790b6cd267063fbfac601cdbe90592a87f50032
JVID <jingduo420@126.com>
2017-05-24 09:15:59 +0800

    5/24

--- src/components/star/star.vue
+++ src/components/star/star.vue
@@ -6,10 +6,10 @@
 </template>
 
 <script type="text/ecmascript-6">
-const LENGTH = 5;
-const CLS_ON = 'on';
-const CLS_OFF = 'off';
-const CLS_HALF = 'half';
+	const LENGTH = 5;
+	const CLS_ON = 'on';
+	const CLS_OFF = 'off';
+	const CLS_HALF = 'half';
 	export default {
 		props: {
 			size: {
@@ -37,7 +37,7 @@ const CLS_HALF = 'half';
 			itemClasses() {
 				let result = [];
 				let score = Math.floor(this.score * 2) / 2;	
-				let hasDecimal = score % 1 !== 0;
+				let hasDecimal = this.score % 1 !== 0;
 				let integer = Math.floor(score);
 				for(let i=0;i<integer;i++){
 					result.push(CLS_ON);
